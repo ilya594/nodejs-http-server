f0884bc18bc7d9ed714857698fda6be7e2203ab6
ilya <cankillah@gmail.com>
2024-06-10 01:38:31 +0300

    apply cors

--- app.js
+++ app.js
@@ -3,27 +3,37 @@
 const fastify = require('fastify');
 const app = fastify();
 const fs = require('fs');
+const cors = require('@fastify/cors');
 
 const port = process.env.port || 8000;
 const host = '0.0.0.0';
 
-app.post('/snapshot', async (req, _) => {
+app.register(cors, { 
+  origin: false,
+}).then(() => {
 
-  fs.writeFileSync('../../../../var/data/snapshots/' + new Date().toDateString() + '.png', req.body?.image);
 
-  return {
-    error: false,
-  };
+  app.post('/snapshot', async (req, reply) => {
+    fs.writeFileSync('../../../../var/data/snapshots/' + new Date().toDateString() + '.png', req.body?.image);
+  
+    reply.send({
+      error: false,
+    });
+  });
+  
+  app.get('/snapshot', async (req, reply) => {  
+    reply.send({
+      error: false,
+    });
+  });
+  
+  app.listen({ port: port, host: host}).then(() => {
+    console.log('Server running ...');
+  });
+
+
 });
 
-app.get('/snapshot', async (req, _) => {  
-  return {
-    error: false,
-  }
-});
 
-app.listen({ port: port, host: host}).then(() => {
-  console.log('Server running ...');
-});
 
 
