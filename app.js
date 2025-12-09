07983e18f449101217c5d42502edd3088fd7142b
Ilya <cankillah@gmail.com>
2025-12-09 04:47:44 +0200

    add peers crud

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ const year = '2025';
 const defaultPath = './../../../../var/data/snapshots/';
 
 const monthMap = {
-  '01': 'January', '02': 'February', '03': 'March', 
-  '04': 'April', '05': 'May', '06': 'June', 
-  '07': 'July', '08': 'August', '09': 'September', 
+  '01': 'January', '02': 'February', '03': 'March',
+  '04': 'April', '05': 'May', '06': 'June',
+  '07': 'July', '08': 'August', '09': 'September',
   '10': 'October', '11': 'November', '12': 'December'
 };
 
@@ -45,6 +45,8 @@ const getPath = (fileName) => {
   const month = String(fileName).substring(3, 5);
   return path.join(defaultPath, year, monthMap[month], fileName);
 }
+
+var peers = [];
 
 const app = express();
 
@@ -75,14 +77,14 @@ app.post('/snapshot', async (request, response) => {
   const fullPath = path.resolve(filePath);
 
   handlePath(fullPath);
-  
+
 
   console.log('writing file buffer to disk: path: ' + fullPath);
 
   fs.writeFile(fullPath, buffer, () => {
     response.send(JSON.stringify({
       error: false,
-    }));  
+    }));
   });
 });
 
@@ -124,7 +126,7 @@ app.get('/delsnapshot', async (request, response) => {
     response.send({
       data: !Boolean(error)
     });
-  });  
+  });
 });
 
 
@@ -146,7 +148,7 @@ app.get('/ls', async (request, response) => {
 app.get('/lsall', async (request, response) => {
 
   if (!await validatePin(request.query.pin)) return response.sendStatus(400);
-  
+
   console.log('app get: lsall');
 
   const folders = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -155,7 +157,7 @@ app.get('/lsall', async (request, response) => {
 
     const folderPath = path.join(defaultPath, year, folder);
 
-    console.log('reading: ' + folderPath);    
+    console.log('reading: ' + folderPath);
 
     handlePath(folderPath + '/dummy.file');
 
@@ -186,12 +188,12 @@ app.get('/valprediction', async (request, response) => {
   if (!request.body || ! await validatePin(request.query.pin)) return response.sendStatus(400);
 
   const prediction = request.query.prediction?.pop();
- 
+
   const result = prediction.value === String(auth);
 
   response.send(JSON.stringify({
     data: result,
-  }));  
+  }));
 });
 
 app.get('/login', async (request, response) => {
@@ -203,8 +205,36 @@ app.get('/login', async (request, response) => {
   });
 });
 
+app.get('/getpeersids', async (request, response) => {
 
-  
+  if (!request.body || !await validatePin(request.query.pin)) return response.sendStatus(400);
+
+  response.send({
+    data: peers,
+  });
+});
+
+app.post('/addpeerid', async (request, response) => {
+
+  if (!request.body) return response.sendStatus(400);
+
+  const id = request.body.id;
+
+  if (id) {
+    peers.push(id);
+    response.send(JSON.stringify({
+      error: false,
+    }));
+  } else {
+    response.send(JSON.stringify({
+      error: 'no id provided',
+    }));
+  }
+
+});
+
+
+
 app.listen(port, async () => {
   console.log('server listening to port: ' + port);
 });
